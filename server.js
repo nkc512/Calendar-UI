0222dc61bf79535908bcb95ad7d3f724285edc3c
nkc512 <nkc512@gmail.com>
2021-08-31 12:06:53 +0530

    AppName updated in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/calendar-ui'));
+app.use(express.static('./dist/Calendar-UI'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/calendar-ui/'}),
+    res.sendFile('index.html', {root: 'dist/Calendar-UI/'}),
 );
 
 // Start the app by listening on the default Heroku port
