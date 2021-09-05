9cbbee57011c76dc4f211fa641a96d66419379ec
nkc512 <nkc512@gmail.com>
2021-09-05 22:36:24 +0530

    Compression removed

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const path = require('path');
 //compression is not working, still kept code to be edited later
-const compression = require('compression')
+//const compression = require('compression')
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/Calendar-UI'));
-app.use(compression())
+
+//app.use(compression())
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/Calendar-UI/'}),
 );
